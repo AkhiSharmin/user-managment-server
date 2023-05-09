b4290a59fdaaf99e330352aa5ed7d76e5076a56e
AkhiSharmin <akhisharmin100@gmail.com>
2023-05-09 22:18:45 +0600

    updated

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
+//middleware
+app.use(cors());
 
-const user = [
+
+const users = [
     { id: 1, name: 'adil', email: 'adil@gmail.com' },
     { id: 2, name: 'john', email: 'jhon@gmail.com' },
-    { id: 3, name: 'maria', email: 'maria@gmail.com' }
+    { id: 3, name: 'maria', email: 'maria@gmail.com' },
 ]
 
 app.get('/', (req, res) => {
     res.send('User management server running')
 })
 
-app.get('/user', (res, req) => {
-    res.send(user);
+app.get('/users', (req, res) => {
+    res.send(users);
 })
 
 app.listen(port, () => {
