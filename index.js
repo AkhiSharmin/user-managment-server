7871c67697d72609915f5cddf8ba01d147d06edc
AkhiSharmin <akhisharmin100@gmail.com>
2023-05-09 21:20:59 +0600

    added data

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+const user = [
+    { id: 1, name: 'adil', email: 'adil@gmail.com' },
+    { id: 2, name: 'john', email: 'jhon@gmail.com' },
+    { id: 3, name: 'maria', email: 'maria@gmail.com' }
+]
+
 app.get('/', (req, res) => {
     res.send('User management server running')
+})
+
+app.get('/user', (res, req) => {
+    res.send(user);
 })
 
 app.listen(port, () => {
